99d9d5e78e669b404df727f65fd5e6be3e1ef62d
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-08-14 11:19:39 -0600

    set up migrations and installed dependencies

--- data/migrations/20200814090719_new_table.js
+++ data/migrations/20200814090719_new_table.js
@@ -1,8 +1,65 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", tbl => {
+      tbl.increments("id");
 
-exports.up = function(knex) {
-  
+      tbl.string("project_name", 255).notNullable();
+      tbl.text("project_description");
+      tbl.boolean("completed_project").defaultTo(false);
+    })
+
+    .createTable("resources", tbl => {
+      tbl.increments("id");
+
+      tbl.string("resource_name", 255).notNullable().unique();
+      tbl.text("resource_description");
+    })
+
+    .createTable("tasks", tbl => {
+      tbl.increments("id");
+
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("projects.id")
+        .onUpdate("CASCADE")
+        .onDelete("RESTRICT");
+
+      tbl.string("task_name", 255).notNullable();
+
+      tbl.text("task_description").notNullable();
+
+      tbl.text("task_notes");
+
+      tbl.boolean("completed_task").defaultTo(false);
+    })
+
+    .createTable("project_resources", tbl => {
+      tbl.increments("id");
+
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("projects.id")
+        .onUpdate("CASCADE")
+        .onDelete("RESTRICT");
+
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("resources.id")
+        .onUpdate("CASCADE")
+        .onDelete("RESTRICT");
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
 };
